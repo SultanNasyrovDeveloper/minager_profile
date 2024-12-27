d83ab786660630337afe8618e2d36d98fc4b42f1
Sultan Nasyrov <tightbtctrader@gmail.com>
2024-12-27 12:13:38 +0300

    Set logging configurations for uvicorn.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from profile.settings import ApplicationSettings
+
 import uvicorn
 
 if __name__ == "__main__":
+    settings = ApplicationSettings()
     uvicorn.run(
         "profile.app:app",
         host="0.0.0.0",
         port=8000,
-        reload=True,
-        log_level="debug",
+        reload=settings.debug,
+        log_level=settings.logging.level,
+        log_config=settings.logging.to_uvicorn_config(),
     )
--- profile/settings.py
+++ profile/settings.py
@@ -46,9 +46,52 @@ class LoggingConfig(BaseSettings):
 
     def to_basic_config(self) -> dict:
         return {
+            "version": 1,
             "level": self.level,
             "format": self.format,
             # 'datefmt': self.datetime_format
+        }
+
+    def to_uvicorn_config(self):
+        return {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "default": {
+                    "()": "uvicorn.logging.DefaultFormatter",
+                    "fmt": self.format,
+                    "use_colors": None,
+                },
+                "access": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "fmt": self.format,  # noqa: E501
+                },
+            },
+            "handlers": {
+                "default": {
+                    "formatter": "default",
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://sys.stderr",
+                },
+                "access": {
+                    "formatter": "access",
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://sys.stdout",
+                },
+            },
+            "loggers": {
+                "uvicorn": {
+                    "handlers": ["default"],
+                    "level": "INFO",
+                    "propagate": False,
+                },
+                "uvicorn.error": {"level": "INFO"},
+                "uvicorn.access": {
+                    "handlers": ["access"],
+                    "level": "INFO",
+                    "propagate": False,
+                },
+            },
         }
 
 
